1ec790f75a94626ad04313a15ced4107a699490f
adriasch-star <adrianschimizzi1@gmail.com>
2026-06-06 09:06:09 +0800

    Revert "supabase install"
    This reverts commit 115b30bd39ec0bfe0032688f97d483042f3f742c.

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center">
       <h1>Trackd co</h1>
-      <button> click me </button>
     </div>
   );
 }
