115b30bd39ec0bfe0032688f97d483042f3f742c
adriasch-star <adrianschimizzi1@gmail.com>
2026-06-06 09:01:03 +0800

    supabase install

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center">
       <h1>Trackd co</h1>
+      <button> click me </button>
     </div>
   );
 }
